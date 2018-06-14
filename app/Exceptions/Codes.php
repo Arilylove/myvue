10b593f0bbb6b4bdeb95cea708a6698835c9d915
Arilylove <1957346813@qq.com>
2018-06-14 10:39:31 +0800

    start

---
+++ app/Exceptions/Codes.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: HXL
+ * Date: 2018/6/12
+ * Time: 15:38
+ */
+namespace app\Exceptions;
+
+class Codes{
+    //返回代码
+    Const system_ok   = 200;
+    Const system_fail = 400;
+
+
+    /**
+     * 用户不存在（未找到用户）
+     */
+    Const user_unexisted = 201;
+    /**
+     * 用户已存在
+     */
+    Const user_existed   = 202;
+    /**
+     * 密码错误
+     */
+    Const pass_wrong     = 203;
+    /**
+     * 验证码错误
+     */
+    Const code_wrong     = 204;
+
+    /**
+     * 退出登录
+     */
+    Const log_out = 205;
+
+
+}
---
+++ app/Exceptions/Msg.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: HXL
+ * Date: 2018/6/12
+ * Time: 15:54
+ */
+namespace app\Exceptions;
+
+class Msg{
+
+    /**
+     * 通用
+     */
+    Const add_ok    = "添加成功";
+    Const add_fail  = "添加失败";
+    Const edit_ok   = "修改成功";
+    Const edit_fail = '修改失败';
+    Const del_ok    = "删除成功";
+    Const del_fail  = '删除失败';
+    Const login_ok = '登录成功';
+
+    Const user_unexisted = '用户不存在';
+    Const pass_wrong = '密码错误';
+    Const code_wrong = '验证码错误';
+
+
+
+}
