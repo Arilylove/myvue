d5e632e2e35ef75e4d63df8e5fec18ea9ab37372
Arilylove <1957346813@qq.com>
2018-07-23 09:27:55 +0800

    start

---
+++ mews/Captcha/CaptchaServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: HXL
+ * Date: 2018/6/15
+ * Time: 16:59
+ */
+namespace Mews\Captcha;
+
+class CaptchaServiceProvider{
+
+}
---
+++ mews/Captcha/Fecades/Captcha.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: HXL
+ * Date: 2018/6/15
+ * Time: 17:02
+ */
+namespace Mews\Captcha\Fecades;
+
+class Captcha{
+
+
+}
